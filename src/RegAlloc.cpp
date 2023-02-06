605cbdeed4872d01edd5095b2ec46c8e5564d817
SophieNyah <sophie.nyascimento@gmail.com>
2023-02-06 01:53:06 -0300

    Alocação de registradores basica implementada

--- src/RegAlloc.cpp
+++ src/RegAlloc.cpp
@@ -64,21 +64,62 @@ struct RegAlloc::Private {
         }
     }
 
-    static void killInterval() {
+    using reg_name = string;
+    using name_variable = std::pair<reg_name, RegAlloc::variable>;
 
+    static void insert_actives(vector<name_variable> &actives, name_variable var) {
+        auto position = std::find_if(actives.begin(), actives.end(), [var](name_variable v)->bool { return v.second.range.death > var.second.range.death; });
+        actives.insert(position, var);
     }
 
-    static void spill() {
+    static void remove_actives(vector<name_variable> &actives, name_variable var) {
+        auto eraser_function = [var](name_variable v)-> bool { return v.first == var.first; };
+        actives.erase(std::remove_if(actives.begin(), actives.end(), eraser_function), actives.end());
+    }
 
+    static void killInterval(vector<name_variable> &actives, vector<string> &avaialble_registers, vector<string> &used_registers, RegAlloc::variable &var) {
+        for(name_variable curr_var: actives) {
+            if(curr_var.second.range.death >= var.range.birth){
+                return;
+            } else {
+                remove_actives(actives, curr_var);
+                used_registers.erase(std::remove(used_registers.begin(), used_registers.end(), curr_var.first), used_registers.end());
+                avaialble_registers.emplace_back(curr_var.first);
+                return;
+            }
+        }
+    }
+
+    static void spill(vector<name_variable> &actives, vector<RegAlloc::variable> &memory_registers, RegAlloc::variable &var) {
+        name_variable spilled{ actives.back() };
+        if(spilled.second.range.death > var.range.death) {
+            memory_registers.emplace_back(spilled.second);
+            actives.pop_back();
+            insert_actives(actives, std::make_pair(spilled.first, var));
+        }
     }
 
     static void linearScan() {
-        vector<int> actives{};
+        vector<name_variable> actives{};
         vector<RegAlloc::variable> intervals{};
-        for(auto var: RegAlloc::variables) { intervals.push_back(var.second); }
-        std::sort(intervals.begin(), intervals.end(), [](RegAlloc::variable& i1, RegAlloc::variable& i2) -> bool { return i1.range.birth < i2.range.birth; });
+        vector<string> available_registers{ RegAlloc::registers };
+        vector<string> used_registers{};
+        vector<RegAlloc::variable> memory_registers{};
 
-        for(auto i: intervals){ std::cout << i.name << '[' << i.range.birth << ',' << i.range.death << "]\n"; }
+        for(auto var: RegAlloc::variables) { intervals.push_back(var.second); }
+        auto comparator_function = [](RegAlloc::variable& i1, RegAlloc::variable& i2) -> bool { return i1.range.birth < i2.range.birth; };
+        std::sort(intervals.begin(), intervals.end(), comparator_function);
+
+        for(auto i: intervals) {
+            killInterval(actives, available_registers, used_registers, i);
+            if(actives.size() == RegAlloc::registers.size()){
+                spill(actives, memory_registers, i);
+            } else {
+                insert_actives(actives, std::make_pair(available_registers.back(), i));
+                available_registers.pop_back();
+            }
+            for(auto a: actives){ std::cout << '[' << a.first << ':' << a.second.name << ' ' << a.second.range.birth << ',' << a.second.range.death << ']' << "  " ; } std::cout << "\n\n";
+        }
     }
 };
 
@@ -90,6 +131,6 @@ void RegAlloc::allocate() {
 
 void RegAlloc::emitCode() {}
 
-void RegAlloc::_newReg(string name) { RegAlloc::registers.push_back(name); }
+void RegAlloc::_newReg(string name) { RegAlloc::registers.push_back(name); };
 void RegAlloc::newInstruction(Instruction i) { RegAlloc::instructions.push_back(i); };
 void RegAlloc::newInstruction(string template_string, vector<string> operands) { RegAlloc::instructions.push_back(Instruction{template_string, operands}); };
